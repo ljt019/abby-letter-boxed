f7f765348f216555287bc44c3d1b822b40e0063d
Lucien Thomas <lthomas@sciport.org>
2024-04-01 14:37:06 -0500

    [Fix] Fixed Configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import MillionLint from "@million/lint";
+import million from "million/compiler";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), MillionLint.vite(), million.vite({ auto: true })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
