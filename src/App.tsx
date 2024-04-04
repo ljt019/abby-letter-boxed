7e1fec900f11ee66860722a70a67901553840970
Lucien Thomas <lthomas@sciport.org>
2024-04-04 09:33:18 -0500

    style: added semicolon after selected letters

--- src/App.tsx
+++ src/App.tsx
@@ -40,7 +40,7 @@ function AppContent() {
   return (
     <div className="flex h-screen flex-col items-center justify-center overflow-hidden">
       <div className="mb-4">
-        <h1>Selected Letters</h1>
+        <h1>Selected Letters:</h1>
         <div>
           {selectedLetters.map((letter, index) => (
             <span key={index} className="mr-2">
